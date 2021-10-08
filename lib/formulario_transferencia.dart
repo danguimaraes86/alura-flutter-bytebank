573c81bd0e44ee70bf76f28f1c435ab8400d6d9e
Daniel Guimarães <danguimaraes86@gmail.com>
2021-10-08 17:20:05 -0300

    add: form design

--- lib/formulario_transferencia.dart
+++ lib/formulario_transferencia.dart
@@ -6,11 +6,39 @@ class FormularioTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Nova Transferência'),
-        ),
-        body: Container(
-          child: Text('Formulário'),
-        ));
+      appBar: AppBar(
+        title: const Text('Nova Transferência'),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.text_fields),
+                labelText: 'Descrição',
+                hintText: 'Insira uma descrição',
+              ),
+              keyboardType: TextInputType.text,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: 'Insira o valor. Ex.: 0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Confirmar'),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class Bytebank extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      home: ListaDeTransferencias(),
+      home: FormularioTransferencia(),
       debugShowCheckedModeBanner: false,
     );
   }
