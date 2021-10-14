99de435dd80c8a8859dd615d2041ff191f3f98f1
Daniel Guimarães <danguimaraes86@gmail.com>
2021-10-14 20:45:13 -0300

    add: FormEditor generic component

--- lib/formulario_transferencia.dart
+++ lib/formulario_transferencia.dart
@@ -1,12 +1,21 @@
+import 'package:alura_bytebank/lista_de_tranferencias.dart';
 import 'package:flutter/material.dart';
 
 class FormularioTransferencia extends StatelessWidget {
   FormularioTransferencia({Key? key}) : super(key: key);
 
-  final TextEditingController _textEditingControllerDescricao =
-      TextEditingController();
-  final TextEditingController _textEditingControllerValor =
-      TextEditingController();
+  final TextEditingController _descricaoController = TextEditingController();
+  final TextEditingController _valorController = TextEditingController();
+
+  void criarNovaTransferencia(descricao, valor) {
+    if (descricao.toString().isNotEmpty &&
+        valor.toString().isNotEmpty &&
+        double.tryParse(valor) != null) {
+      Transferencia novaTransferencia =
+          Transferencia(double.parse(valor), descricao);
+      print(novaTransferencia);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,41 +25,59 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: TextField(
-              controller: _textEditingControllerDescricao,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.text_fields),
-                labelText: 'Descrição',
-                hintText: 'Insira uma descrição',
-              ),
-              keyboardType: TextInputType.text,
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: TextField(
-              controller: _textEditingControllerValor,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: 'Insira o valor. Ex.: 0.00',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          FormEditor(
+              controller: _descricaoController,
+              icon: Icons.text_fields,
+              label: 'Descrição',
+              hint: 'Insira uma descrição',
+              textInputType: TextInputType.text),
+          FormEditor(
+              controller: _valorController,
+              icon: Icons.monetization_on,
+              label: 'Valor',
+              hint: 'Insira o valor. Ex.: 0.00',
+              textInputType: TextInputType.number),
           ElevatedButton(
             onPressed: () {
-              print('botão clicado');
-              print(_textEditingControllerDescricao.text);
-              print(_textEditingControllerValor.text);
+              criarNovaTransferencia(
+                  _descricaoController.text, _valorController.text);
             },
             child: const Text('Confirmar'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class FormEditor extends StatelessWidget {
+  final TextEditingController controller;
+  final IconData? icon;
+  final String label;
+  final String? hint;
+  final TextInputType? textInputType;
+
+  FormEditor(
+      {required this.controller,
+      this.icon,
+      required this.label,
+      this.hint,
+      this.textInputType,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          icon: Icon(icon),
+          labelText: label,
+          hintText: hint,
+        ),
+        keyboardType: textInputType,
       ),
     );
   }
